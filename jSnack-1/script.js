0b13ceefb2ffe903cccdeb2b1e26cd4822e81476
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-04-04 15:57:13 +0200

    [First commit] + Jsnack1

---
+++ jSnack-1/script.js
@@ -0,0 +1,42 @@
+const users = [
+  {
+    firstName : "Mario",
+    lastName : "Rossi",
+    age : 22
+  },
+  {
+    firstName : "Giuseppe",
+    lastName : "Verdi",
+    age : 66
+  },
+  {
+    firstName : "Luigi",
+    lastName : "De Girolamo",
+    age : 54
+  },
+  {
+    firstName : "Ugo",
+    lastName : "De Ughi",
+    age : 15
+  },
+  {
+    firstName : "Alberto",
+    lastName : "Solenne",
+    age : 90
+  },
+  {
+    firstName : "Umbertino",
+    lastName : "Bianchi",
+    age : 10
+  },
+]
+
+const utentiMinorenni = users.filter( (user) => user.age < 18 )
+console.log(utentiMinorenni)
+
+const utentiMaggiorenne = users.filter( (user) => user.age >= 18 && user.age <= 65 )
+console.log(utentiMaggiorenne)
+
+const utentiOver = users.filter( (user) => user.age > 65 )
+console.log(utentiOver)
+
